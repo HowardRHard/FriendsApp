7cabac25c2ec9ba63b8bea99305b0c117213985d
Unknown <H.Hardy1@uni.brighton.ac.uk>
2018-09-03 20:03:59 +0100

    Testing Glide Library

--- app/src/main/java/com/pinfo/howard/friendsapp/MainActivity.kt
+++ app/src/main/java/com/pinfo/howard/friendsapp/MainActivity.kt
@@ -2,11 +2,22 @@ package com.pinfo.howard.friendsapp
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.pinfo.howard.friendsapp.R.id.lImg
+import com.pinfo.howard.friendsapp.R.id.rImg
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
+
+        GlideApp.with(applicationContext)
+                    .load(applicationContext.getString(R.string.link_left))
+                    .into(lImg)
+
+            GlideApp.with(applicationContext)
+                    .load(applicationContext.getString(R.string.link_right))
+                    .into(rImg)
+        }
 }
